8fab531f38bb6efa0e30dae1cc82856cc794f835
Linas Išganaitis <l.isganaitis@gmail.com>
2022-01-26 16:12:30 +0100

    - HcsDid uses new DID string format; - FileId is not longer used in HcsDid; - DidDocumentBase does not append verificationMethods and authentication parts to the document if didRootKey is missing; - A few TODO comments added;
    TODO:
    - Fix tests
    
    
    Signed-off-by: Derek Munneke (Meeco) <derek.munneke@meeco.me>; Signed-off-by: Derek Munneke <derek.munneke@nextfaze.com>; Signed-off-by: Jan Vereecken <jan.vereecken@meeco.me>; Signed-off-by: Linas Išganaitis <l.isganaitis@gmail.com>; Signed-off-by: Linas Išganaitis <linas.isganaitis@meeco.me>; Signed-off-by: Vijay Shiyani <vshiyani@meeco.me>; Signed-off-by: Zak Barbuto <zbarbuto@nextfaze.com>; Signed-off-by: elena <elena.murova@meeco.me>; Signed-off-by: linasi <l.isganaitis@gmail.com>; Signed-off-by: vijay <vijayshiyani@gmail.com>

--- src/identity/hcs/did/hcs-did.ts
+++ src/identity/hcs/did/hcs-did.ts
@@ -1,19 +1,17 @@
-import {HederaDid} from "../../hedera-did";
-import {DidSyntax} from "../../did-syntax";
-import {FileId, PrivateKey, PublicKey, TopicId} from "@hashgraph/sdk";
-import {Hashing} from "../../../utils/hashing";
-import {DidDocumentBase} from "../../did-document-base";
-import {HcsDidRootKey} from "./hcs-did-root-key";
+import { PrivateKey, PublicKey, TopicId } from "@hashgraph/sdk";
+import { Hashing } from "../../../utils/hashing";
+import { DidDocumentBase } from "../../did-document-base";
+import { DidSyntax } from "../../did-syntax";
+import { HederaDid } from "../../hedera-did";
+import { HcsDidRootKey } from "./hcs-did-root-key";
 
 /**
  * Hedera Decentralized Identifier for Hedera DID Method specification based on HCS.
  */
 export class HcsDid implements HederaDid {
     public static DID_METHOD = DidSyntax.Method.HEDERA_HCS;
-    private static DID_PARAMETER_VALUE_PARTS = 2;
 
     private didTopicId: TopicId;
-    private addressBookFileId: FileId;
     private network: string;
     private idString: string;
     private did: string;
@@ -25,47 +23,41 @@ export class HcsDid implements HederaDid {
      *
      * @param network           The Hedera DID network.
      * @param didRootKey        The public key from which DID is derived.
-     * @param addressBookFileId The appent's address book {@link FileId}
      * @param didTopicId        The appnet's DID topic ID.
      */
-    constructor(network: string, didRootKey: PublicKey, addressBookFileId: FileId, didTopicId?: TopicId);
+    constructor(network: string, didRootKey: PublicKey, didTopicId?: TopicId);
     /**
      * Creates a DID instance with private DID root key.
      *
      * @param network           The Hedera DID network.
      * @param privateDidRootKey The private DID root key.
-     * @param addressBookFileId The appent's address book {@link FileId}
      * @param didTopicId        The appnet's DID topic ID.
      */
-    constructor(network: string, privateDidRootKey: PrivateKey, addressBookFileId: FileId, didTopicId?: TopicId);
+    constructor(network: string, privateDidRootKey: PrivateKey, didTopicId?: TopicId);
     /**
      * Creates a DID instance without topic ID specification.
      *
      * @param network           The Hedera DID network.
      * @param didRootKey        The public key from which DID is derived.
-     * @param addressBookFileId The appent's address book {@link FileId}
      */
-    constructor(network: string, didRootKey: PublicKey, addressBookFileId: FileId)
+    constructor(network: string, didRootKey: PublicKey)
     /**
      * Creates a DID instance.
      *
      * @param network           The Hedera DID network.
      * @param idString          The id-string of a DID.
-     * @param addressBookFileId The appent's address book {@link FileId}
      * @param didTopicId        The appnet's DID topic ID.
      */
-    constructor(network: string, idString: string, addressBookFileId: FileId, didTopicId?: TopicId);
+    constructor(network: string, idString: string, didTopicId?: TopicId);
     constructor(...args: any[]) {
         if (
             (typeof args[0] === 'string') &&
             (args[1] instanceof PublicKey) &&
-            (args[2] instanceof FileId) &&
-            ((args[4] instanceof TopicId) || args[4] === undefined) &&
-            (args.length === 4)
+            ((args[2] instanceof TopicId) || args[2] === undefined) &&
+            (args.length === 3)
         ) {
-            const [network, didRootKey, addressBookFileId, didTopicId] = args;
+            const [network, didRootKey, didTopicId] = args;
             this.didTopicId = didTopicId;
-            this.addressBookFileId = addressBookFileId;
             this.network = network;
             this.didRootKey = didRootKey;
             this.idString = HcsDid.publicKeyToIdString(didRootKey);
@@ -77,14 +69,12 @@ export class HcsDid implements HederaDid {
         if (
             (typeof args[0] === 'string') &&
             (args[1] instanceof PrivateKey) &&
-            (args[2] instanceof FileId) &&
-            ((args[4] instanceof TopicId) || args[4] === undefined) &&
-            (args.length === 4)
+            ((args[2] instanceof TopicId) || args[2] === undefined) &&
+            (args.length === 3)
         ) {
-            const [network, privateDidRootKey, addressBookFileId, didTopicId] = args
+            const [network, privateDidRootKey, didTopicId] = args
 
             this.didTopicId = didTopicId;
-            this.addressBookFileId = addressBookFileId;
             this.network = network;
             this.didRootKey = privateDidRootKey.publicKey;
             this.idString = HcsDid.publicKeyToIdString(privateDidRootKey.publicKey);
@@ -97,13 +87,11 @@ export class HcsDid implements HederaDid {
         if (
             (typeof args[0] === 'string') &&
             (args[1] instanceof PublicKey) &&
-            (args[2] instanceof FileId) &&
-            (args.length === 3)
+            (args.length === 2)
         ) {
-            const [network, didRootKey, addressBookFileId] = args;
+            const [network, didRootKey] = args;
 
             this.didTopicId = null;
-            this.addressBookFileId = addressBookFileId;
             this.network = network;
             this.didRootKey = didRootKey;
             this.idString = HcsDid.publicKeyToIdString(didRootKey);
@@ -112,17 +100,18 @@ export class HcsDid implements HederaDid {
             return;
         }
 
+        /**
+         * TODO: investigate. This version does not fill in didRootKey so DID is missing publicKey information
+         */
         if (
             (typeof args[0] === 'string') &&
             (typeof args[1] === 'string') &&
-            (args[2] instanceof FileId) &&
-            ((args[4] instanceof TopicId) || args[4] === undefined) &&
-            (args.length === 4)
+            ((args[2] instanceof TopicId) || args[2] === undefined) &&
+            (args.length === 3)
         ) {
-            const [network, idString, addressBookFileId, didTopicId] = args;
+            const [network, idString, didTopicId] = args;
 
             this.didTopicId = didTopicId;
-            this.addressBookFileId = addressBookFileId;
             this.network = network;
 
             this.idString = idString;
@@ -145,13 +134,15 @@ export class HcsDid implements HederaDid {
             throw new Error("DID string cannot be null");
         }
 
-        const mainParts = didString.split(DidSyntax.DID_PARAMETER_SEPARATOR);
+        const [didPart, topicIdPart] = didString.split(DidSyntax.DID_TOPIC_SEPARATOR);
 
-        let
-            topicId: TopicId,
-            addressBookFileId: FileId;
+        if (!topicIdPart) {
+            throw new Error("DID string is invalid: topic ID is missing");
+        }
 
-        const didParts = mainParts.shift().split(DidSyntax.DID_METHOD_SEPARATOR);
+        const topicId = TopicId.fromString(topicIdPart)
+
+        const didParts = didPart.split(DidSyntax.DID_METHOD_SEPARATOR);
 
         if (didParts.shift() !== DidSyntax.DID_PREFIX) {
             throw new Error('DID string is invalid: invalid prefix.');
@@ -164,56 +155,17 @@ export class HcsDid implements HederaDid {
 
         try {
             const networkName = didParts.shift();
-
-            const params = this.extractParameters(mainParts, methodName, networkName);
-            addressBookFileId = FileId.fromString(params.get(DidSyntax.MethodSpecificParameter.ADDRESS_BOOK_FILE_ID));
-            if (params.has(DidSyntax.MethodSpecificParameter.DID_TOPIC_ID)) {
-                topicId = TopicId.fromString(params.get(DidSyntax.MethodSpecificParameter.DID_TOPIC_ID));
-            }
             const didIdString = didParts.shift();
+
             if (didIdString.length < 32 || didParts.shift()) {
                 throw new Error('DID string is invalid.')
             }
 
-            return new HcsDid(networkName, didIdString, addressBookFileId, topicId);
+            return new HcsDid(networkName, didIdString, topicId);
 
         } catch (e) {
             throw new Error('DID string is invalid. ' + e.message);
         }
-    }
-
-    /**
-     * Extracts method-specific URL parameters.
-     *
-     * @param mainParts   Iterator over main parts of the DID.
-     * @param methodName  The method name.
-     * @param networkName The network name.
-     * @return A map of method-specific URL parameters and their values.
-     */
-    private static extractParameters(mainParts: string[], methodName: string, networkName: string): Map<string, string> {
-        const result = new Map();
-
-        const fidParamName = [methodName, networkName, DidSyntax.MethodSpecificParameter.ADDRESS_BOOK_FILE_ID].join(DidSyntax.DID_METHOD_SEPARATOR);
-        const tidParamName = [methodName, networkName, DidSyntax.MethodSpecificParameter.DID_TOPIC_ID].join(DidSyntax.DID_METHOD_SEPARATOR);
-
-        let mp;
-        while(mp = mainParts.shift()) {
-            const paramValue = mp.split(DidSyntax.DID_PARAMETER_VALUE_SEPARATOR);
-
-            if (paramValue.length != this.DID_PARAMETER_VALUE_PARTS) {
-                continue;
-            } else if (fidParamName === paramValue[0]) {
-                result.set(DidSyntax.MethodSpecificParameter.ADDRESS_BOOK_FILE_ID, paramValue[1]);
-            } else if (tidParamName === paramValue[0]) {
-                result.set(DidSyntax.MethodSpecificParameter.DID_TOPIC_ID, paramValue[1]);
-            }
-        }
-
-        if (!result.has(DidSyntax.MethodSpecificParameter.ADDRESS_BOOK_FILE_ID)) {
-            throw new Error('DID string is invalid. Required method-specific URL parameter not found: ' + DidSyntax.MethodSpecificParameter.ADDRESS_BOOK_FILE_ID);
-        }
-
-        return result;
     }
 
     /**
@@ -259,10 +211,6 @@ export class HcsDid implements HederaDid {
         return this.didTopicId;
     }
 
-    public getAddressBookFileId(): FileId {
-        return this.addressBookFileId;
-    }
-
     public getIdString(): string {
         return this.idString;
     }
@@ -284,21 +232,11 @@ export class HcsDid implements HederaDid {
             DidSyntax.DID_METHOD_SEPARATOR +
             methodNetwork +
             DidSyntax.DID_METHOD_SEPARATOR +
-            this.idString +
-            DidSyntax.DID_PARAMETER_SEPARATOR +
-            methodNetwork +
-            DidSyntax.DID_METHOD_SEPARATOR +
-            DidSyntax.MethodSpecificParameter.ADDRESS_BOOK_FILE_ID +
-            DidSyntax.DID_PARAMETER_VALUE_SEPARATOR +
-            this.addressBookFileId.toString();
+            this.idString;
 
         if(this.didTopicId) {
             ret = ret +
-                DidSyntax.DID_PARAMETER_SEPARATOR +
-                methodNetwork +
-                DidSyntax.DID_METHOD_SEPARATOR +
-                DidSyntax.MethodSpecificParameter.DID_TOPIC_ID +
-                DidSyntax.DID_PARAMETER_VALUE_SEPARATOR +
+                DidSyntax.DID_TOPIC_SEPARATOR +
                 this.didTopicId.toString();
         }
 
