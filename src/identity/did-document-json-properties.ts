b728e54afda8e4e6444a87da7a2ee2ef78449d49
Linas Išganaitis <l.isganaitis@gmail.com>
2022-01-26 16:12:30 +0100

    - HcsDid uses new DID string format; - FileId is not longer used in HcsDid; - DidDocumentBase does not append verificationMethods and authentication parts to the document if didRootKey is missing; - A few TODO comments added;
    TODO:
    - Fix tests

--- src/identity/did-document-json-properties.ts
+++ src/identity/did-document-json-properties.ts
@@ -2,6 +2,7 @@ export module DidDocumentJsonProperties {
     export const CONTEXT = '@context';
     export const ID = 'id';
     export const AUTHENTICATION = 'authentication';
+    export const VERIFICATION_METHOD = 'verificationMethod';
     export const PUBLIC_KEY = 'publicKey';
     export const SERVICE = 'service';
     export const CREATED = 'created';
