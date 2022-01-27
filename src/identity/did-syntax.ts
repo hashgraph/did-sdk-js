351996416f3f7704900078c357c5dd367bee7101
Vijay Shiyani <vshiyani@meeco.me>
2022-01-27 14:16:00 +1100

    fix hcs did test and added hedera network constant
    Signed-off-by: Derek Munneke <derek.munneke@meeco.me>

--- src/identity/did-syntax.ts
+++ src/identity/did-syntax.ts
@@ -3,6 +3,8 @@ export module DidSyntax {
   export const DID_DOCUMENT_CONTEXT = "https://www.w3.org/ns/did/v1";
   export const DID_METHOD_SEPARATOR = ":";
   export const DID_TOPIC_SEPARATOR = "_";
+  export const HEDERA_NETWORK_MAINNET = "mainnet";
+  export const HEDERA_NETWORK_TESTNET = "testnet";
 
   export enum Method {
     HEDERA_HCS = "hedera",
