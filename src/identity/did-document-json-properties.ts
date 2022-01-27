962a0a2e9416e47b3506725ad3866f625406106f
Vijay Shiyani <vshiyani@meeco.me>
2022-01-27 11:36:40 +1100

    remove public_key from did document
    Signed-off-by: Derek Munneke <derek.munneke@meeco.me>

--- src/identity/did-document-json-properties.ts
+++ src/identity/did-document-json-properties.ts
@@ -1,11 +1,10 @@
 export module DidDocumentJsonProperties {
-    export const CONTEXT = '@context';
-    export const ID = 'id';
-    export const AUTHENTICATION = 'authentication';
-    export const VERIFICATION_METHOD = 'verificationMethod';
-    export const PUBLIC_KEY = 'publicKey';
-    export const SERVICE = 'service';
-    export const CREATED = 'created';
-    export const UPDATED = 'updated';
-    export const PROOF = 'proof';
+    export const CONTEXT = "@context";
+    export const ID = "id";
+    export const AUTHENTICATION = "authentication";
+    export const VERIFICATION_METHOD = "verificationMethod";
+    export const SERVICE = "service";
+    export const CREATED = "created";
+    export const UPDATED = "updated";
+    export const PROOF = "proof";
 }
