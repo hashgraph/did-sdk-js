08a957797b1135bee7a254a54ddf5bd718bf9563
Vijay Shiyani <vshiyani@meeco.me>
2022-01-27 16:26:53 +1100

    fix identity network and related tests

--- src/identity/hcs/hcs-identity-network-builder.ts
+++ src/identity/hcs/hcs-identity-network-builder.ts
@@ -1,30 +1,31 @@
-import {Client, FileCreateTransaction, Hbar, PublicKey, TopicCreateTransaction, TopicId} from "@hashgraph/sdk";
+import {Client, Hbar, PublicKey, TopicCreateTransaction, TopicId} from "@hashgraph/sdk";
 import {HcsIdentityNetwork} from "./hcs-identity-network";
 
 export class HcsIdentityNetworkBuilder {
-    private appnetName: string;
     private didTopicId: TopicId;
     private vcTopicId: TopicId;
     private network: string;
-    private didServers: string[];
     private publicKey: PublicKey;
     private maxTransactionFee: Hbar = new Hbar(2);
     private didTopicMemo: string = '';
     private vcTopicMemo: string = '';
 
     public async execute(client: Client): Promise<HcsIdentityNetwork> {
-        const didTopicCreateTransaction = new TopicCreateTransaction()
+
+
+        if(!this.didTopicId){
+            const didTopicCreateTransaction = new TopicCreateTransaction()
             .setMaxTransactionFee(this.maxTransactionFee)
-            .setTopicMemo(this.didTopicMemo);
-
-        if (this.publicKey) {
-            didTopicCreateTransaction.setAdminKey(this.publicKey);
+            .setTopicMemo(this.didTopicMemo); 
+            if (this.publicKey) {
+                didTopicCreateTransaction.setAdminKey(this.publicKey);
+            }
+            const didTxId = await didTopicCreateTransaction.execute(client);
+            this.didTopicId = (await didTxId.getReceipt(client)).topicId;
         }
-
-        const didTxId = await didTopicCreateTransaction.execute(client);
-        this.didTopicId = (await didTxId.getReceipt(client)).topicId;
-
-        const vcTopicCreateTransaction = new TopicCreateTransaction()
+     
+        if(!this.vcTopicId){
+            const vcTopicCreateTransaction = new TopicCreateTransaction()
             .setMaxTransactionFee(this.maxTransactionFee)
             .setTopicMemo(this.vcTopicMemo);
 
@@ -34,26 +35,12 @@ export class HcsIdentityNetworkBuilder {
 
         const vcTxId = await vcTopicCreateTransaction.execute(client);
         this.vcTopicId = (await vcTxId.getReceipt(client)).topicId;
-
-        return HcsIdentityNetwork.fromHcsDid(this.network, this.didTopicId, this.vcTopicId);
-    }
-
-    public addAppnetDidServer(serverUrl: string): HcsIdentityNetworkBuilder {
-        if (!this.didServers) {
-            this.didServers = [];
         }
 
-        if (this.didServers.indexOf(serverUrl) == -1) {
-            this.didServers.push(serverUrl);
-        }
 
-        return this;
+        return HcsIdentityNetwork.fromHcsDidAndVCTopic(this.network, this.didTopicId, this.vcTopicId);
     }
 
-    public setAppnetName(appnetName: string): HcsIdentityNetworkBuilder {
-        this.appnetName = appnetName;
-        return this;
-    }
 
     public setDidTopicMemo(didTopicMemo: string): HcsIdentityNetworkBuilder {
         this.didTopicMemo = didTopicMemo;
