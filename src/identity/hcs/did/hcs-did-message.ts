fc4b94cc4759cd56c9f9c0bb535cbee2b0bd0519
Linas Išganaitis <l.isganaitis@gmail.com>
2022-01-31 19:45:38 +0100

    Trying to integrate events
    Signed-off-by: Derek Munneke (Meeco) <derek.munneke@meeco.me>; Signed-off-by: Derek Munneke <derek.munneke@nextfaze.com>; Signed-off-by: Jan Vereecken <jan.vereecken@meeco.me>; Signed-off-by: Linas Išganaitis <l.isganaitis@gmail.com>; Signed-off-by: Linas Išganaitis <linas.isganaitis@meeco.me>; Signed-off-by: Vijay Shiyani <vshiyani@meeco.me>; Signed-off-by: Zak Barbuto <zbarbuto@nextfaze.com>; Signed-off-by: elena <elena.murova@meeco.me>; Signed-off-by: linasi <l.isganaitis@gmail.com>; Signed-off-by: vijay <vijayshiyani@gmail.com>

--- src/identity/hcs/did/hcs-did-message.ts
+++ src/identity/hcs/did/hcs-did-message.ts
@@ -1,11 +1,7 @@
-import { PublicKey, Timestamp, TopicId } from "@hashgraph/sdk";
-import { Hashing } from "../../../utils/hashing";
-import { TimestampUtils } from "../../../utils/timestamp-utils";
-import { DidDocumentBase } from "../../did-document-base";
-import { DidDocumentJsonProperties } from "../../did-document-json-properties";
+import { Timestamp, TopicId } from "@hashgraph/sdk";
 import { DidMethodOperation } from "../../did-method-operation";
-import { Decrypter, Encrypter, Message } from "../message";
-import { MessageEnvelope } from "../message-envelope";
+import { Message } from "../message";
+import { HcsDidEvent } from "./event/hcs-did-event";
 import { HcsDid } from "./hcs-did";
 
 /**
@@ -14,7 +10,7 @@ import { HcsDid } from "./hcs-did";
 export class HcsDidMessage extends Message {
     protected operation: DidMethodOperation;
     protected did: string;
-    protected didDocumentBase64: string;
+    protected event: HcsDidEvent;
     /**
      * The date when the DID was created and published.
      * It is equal to consensus timestamp of the first creation message.
@@ -36,11 +32,11 @@ export class HcsDidMessage extends Message {
      * @param did               The DID string.
      * @param didDocumentBase64 The Base64-encoded DID document.
      */
-    constructor(operation: DidMethodOperation, did: string, didDocumentBase64: string) {
+    constructor(operation: DidMethodOperation, did: string, event: HcsDidEvent) {
         super();
         this.operation = operation;
         this.did = did;
-        this.didDocumentBase64 = didDocumentBase64;
+        this.event = event;
     }
 
     public getOperation(): DidMethodOperation {
@@ -51,8 +47,12 @@ export class HcsDidMessage extends Message {
         return this.did;
     }
 
-    public getDidDocumentBase64(): string {
-        return this.didDocumentBase64;
+    public getEvent(): HcsDidEvent {
+        return this.event;
+    }
+
+    public getEventBase64() {
+        return this.getEvent().getBase64();
     }
 
     public getCreated(): Timestamp {
@@ -72,38 +72,6 @@ export class HcsDidMessage extends Message {
     }
 
     /**
-     * Decodes didDocumentBase64 field and returns its content.
-     * In case this message is in encrypted mode, it will return encrypted content,
-     * so getPlainDidDocument method should be used instead.
-     * If message consensus timestamps for creation and update are provided they will be injected into the result
-     * document upon decoding.
-     *
-     * @return The decoded DID document as JSON string.
-     */
-    public getDidDocument(): string {
-        if (this.didDocumentBase64 == null) {
-            return null;
-        }
-
-        let document: string = Hashing.base64.decode(this.didDocumentBase64);
-
-        // inject timestamps
-        if (this.created != null || this.updated != null) {
-            const root = JSON.parse(document);
-            if (this.created != null) {
-                root[DidDocumentJsonProperties.CREATED] = TimestampUtils.toJSON(this.created);
-            }
-
-            if (this.updated != null) {
-                root[DidDocumentJsonProperties.UPDATED] = TimestampUtils.toJSON(this.updated);
-            }
-            document = JSON.stringify(root);
-        }
-
-        return document;
-    }
-
-    /**
      * Validates this DID message by checking its completeness, signature and DID document.
      *
      * @return True if the message is valid, false otherwise.
@@ -118,33 +86,38 @@ export class HcsDidMessage extends Message {
     public isValid(didTopicId: TopicId): boolean;
     public isValid(...args: any[]): boolean {
         const didTopicId: TopicId = args[0] || null;
-        if (this.did == null || this.didDocumentBase64 == null) {
+
+        if (this.did == null || this.event == null) {
             return false;
         }
 
+        /**
+         * TODO: review this logic and update accoridingly
+         */
+
         try {
-            const doc: DidDocumentBase = DidDocumentBase.fromJson(this.getDidDocument());
+            // const doc: DidDocumentBase = DidDocumentBase.fromJson(this.getDidDocument());
 
-            // Validate if DID and DID document are present and match
-            if (this.did != doc.getId()) {
-                return false;
-            }
+            // // Validate if DID and DID document are present and match
+            // if (this.did != doc.getId()) {
+            //     return false;
+            // }
 
-            // Validate if DID root key is present in the document
-            if (doc.getDidRootKey() == null || doc.getDidRootKey().getPublicKeyMultibase() == null) {
-                return false;
-            }
+            // // Validate if DID root key is present in the document
+            // if (doc.getDidRootKey() == null || doc.getDidRootKey().getPublicKeyMultibase() == null) {
+            //     return false;
+            // }
 
-            // Verify that DID was derived from this DID root key
+            // // Verify that DID was derived from this DID root key
             const hcsDid: HcsDid = HcsDid.fromString(this.did);
 
-            // Extract public key from the DID document
-            const publicKeyBytes: Uint8Array = Hashing.multibase.decode(doc.getDidRootKey().getPublicKeyMultibase());
-            const publicKey: PublicKey = PublicKey.fromBytes(publicKeyBytes);
+            // // Extract public key from the DID document
+            // const publicKeyBytes: Uint8Array = Hashing.multibase.decode(doc.getDidRootKey().getPublicKeyMultibase());
+            // const publicKey: PublicKey = PublicKey.fromBytes(publicKeyBytes);
 
-            if (HcsDid.publicKeyToIdString(publicKey) != hcsDid.getIdString()) {
-                return false;
-            }
+            // if (HcsDid.publicKeyToIdString(publicKey) != hcsDid.getIdString()) {
+            //     return false;
+            // }
 
             // Verify that the message was sent to the right topic, if the DID contains the topic
             if (
@@ -166,30 +139,33 @@ export class HcsDidMessage extends Message {
      *
      * @return Public key of the DID subject.
      */
-    public extractDidRootKey(): PublicKey {
-        let result: PublicKey = null;
-        try {
-            const doc: DidDocumentBase = DidDocumentBase.fromJson(this.getDidDocument());
-            // Make sure that DID root key is present in the document
-            if (doc.getDidRootKey() != null && doc.getDidRootKey().getPublicKeyMultibase() != null) {
-                const publicKeyBytes: Uint8Array = Hashing.multibase.decode(
-                    doc.getDidRootKey().getPublicKeyMultibase()
-                );
-                result = PublicKey.fromBytes(publicKeyBytes);
-            }
-            // ArrayIndexOutOfBoundsException is thrown in case public key is invalid in PublicKey.fromBytes
-        } catch (e) {
-            return null;
-        }
+    /**
+     * TODO: message no longer contains the whole DID document
+     */
+    // public extractDidRootKey(): PublicKey {
+    //     let result: PublicKey = null;
+    //     try {
+    //         const doc: DidDocumentBase = DidDocumentBase.fromJson(this.getDidDocument());
+    //         // Make sure that DID root key is present in the document
+    //         if (doc.getDidRootKey() != null && doc.getDidRootKey().getPublicKeyMultibase() != null) {
+    //             const publicKeyBytes: Uint8Array = Hashing.multibase.decode(
+    //                 doc.getDidRootKey().getPublicKeyMultibase()
+    //             );
+    //             result = PublicKey.fromBytes(publicKeyBytes);
+    //         }
+    //         // ArrayIndexOutOfBoundsException is thrown in case public key is invalid in PublicKey.fromBytes
+    //     } catch (e) {
+    //         return null;
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
     public toJsonTree(): any {
         const result: any = super.toJsonTree();
         result.operation = this.operation;
         result.did = this.did;
-        result.didDocumentBase64 = this.didDocumentBase64;
+        result.event = this.getEventBase64();
         return result;
     }
 
@@ -199,7 +175,7 @@ export class HcsDidMessage extends Message {
         } else {
             result.operation = tree.operation;
             result.did = tree.did;
-            result.didDocumentBase64 = tree.didDocumentBase64;
+            // result.didDocumentBase64 = tree.didDocumentBase64;
         }
         result = super.fromJsonTree(tree, result) as HcsDidMessage;
         return result;
@@ -220,15 +196,18 @@ export class HcsDidMessage extends Message {
      * @param operation       The operation on DID document.
      * @return The HCS message wrapped in an envelope for the given DID document and method operation.
      */
-    public static fromDidDocumentJson(
-        didDocumentJson: string,
-        operation: DidMethodOperation
-    ): MessageEnvelope<HcsDidMessage> {
-        const didDocumentBase: DidDocumentBase = DidDocumentBase.fromJson(didDocumentJson);
-        const didDocumentBase64 = Hashing.base64.encode(didDocumentJson);
-        const message: HcsDidMessage = new HcsDidMessage(operation, didDocumentBase.getId(), didDocumentBase64);
-        return new MessageEnvelope<HcsDidMessage>(message);
-    }
+    /**
+     * TODO: we no longer submit the whole DID document
+     */
+    // public static fromDidDocumentJson(
+    //     didDocumentJson: string,
+    //     operation: DidMethodOperation
+    // ): MessageEnvelope<HcsDidMessage> {
+    //     const didDocumentBase: DidDocumentBase = DidDocumentBase.fromJson(didDocumentJson);
+    //     const didDocumentBase64 = Hashing.base64.encode(didDocumentJson);
+    //     const message: HcsDidMessage = new HcsDidMessage(operation, didDocumentBase.getId(), didDocumentBase64);
+    //     return new MessageEnvelope<HcsDidMessage>(message);
+    // }
 
     /**
      * Provides an encryption operator that converts an {@link HcsDidMessage} into encrypted one.
@@ -236,21 +215,21 @@ export class HcsDidMessage extends Message {
      * @param encryptionFunction The encryption function to use for encryption of single attributes.
      * @return The encryption operator instance.
      */
-    public static getEncrypter(encryptionFunction: Encrypter<string>): Encrypter<HcsDidMessage> {
-        if (encryptionFunction == null) {
-            throw "Encryption function is missing or null.";
-        }
-        return function (message: HcsDidMessage): HcsDidMessage {
-            const operation: DidMethodOperation = message.getOperation();
-            // Encrypt the DID
-            const encryptedDid: string = encryptionFunction(message.getDid());
-            const did: string = Hashing.base64.encode(encryptedDid);
-            // Encrypt the DID document
-            const encryptedDoc: string = encryptionFunction(message.getDidDocumentBase64());
-            const didDocumentBase64: string = Hashing.base64.encode(encryptedDoc);
-            return new HcsDidMessage(operation, did, didDocumentBase64);
-        };
-    }
+    // public static getEncrypter(encryptionFunction: Encrypter<string>): Encrypter<HcsDidMessage> {
+    //     if (encryptionFunction == null) {
+    //         throw "Encryption function is missing or null.";
+    //     }
+    //     return function (message: HcsDidMessage): HcsDidMessage {
+    //         const operation: DidMethodOperation = message.getOperation();
+    //         // Encrypt the DID
+    //         const encryptedDid: string = encryptionFunction(message.getDid());
+    //         const did: string = Hashing.base64.encode(encryptedDid);
+    //         // Encrypt the DID document
+    //         const encryptedDoc: string = encryptionFunction(message.getDidDocumentBase64());
+    //         const didDocumentBase64: string = Hashing.base64.encode(encryptedDoc);
+    //         return new HcsDidMessage(operation, did, didDocumentBase64);
+    //     };
+    // }
 
     /**
      * Provides a decryption function that converts {@link HcsDidMessage} in encrypted for into a plain form.
@@ -258,25 +237,25 @@ export class HcsDidMessage extends Message {
      * @param decryptionFunction The decryption function to use for decryption of single attributes.
      * @return The Decryption function for the {@link HcsDidMessage}
      */
-    public static getDecrypter(decryptionFunction: Decrypter<string>): Decrypter<HcsDidMessage> {
-        if (decryptionFunction == null) {
-            throw "Decryption function is missing or null.";
-        }
-        return function (encryptedMsg: HcsDidMessage, consensusTimestamp: Timestamp): HcsDidMessage {
-            const operation: DidMethodOperation = encryptedMsg.getOperation();
-            // Decrypt DID string
-            let decryptedDid: string = encryptedMsg.getDid();
-            if (decryptedDid != null) {
-                const did: string = Hashing.base64.decode(decryptedDid);
-                decryptedDid = decryptionFunction(did, consensusTimestamp);
-            }
-            // Decrypt DID document
-            let decryptedDocBase64 = encryptedMsg.getDidDocumentBase64();
-            if (decryptedDocBase64 != null) {
-                const doc: string = Hashing.base64.decode(decryptedDocBase64);
-                decryptedDocBase64 = decryptionFunction(doc, consensusTimestamp);
-            }
-            return new HcsDidMessage(operation, decryptedDid, decryptedDocBase64);
-        };
-    }
+    // public static getDecrypter(decryptionFunction: Decrypter<string>): Decrypter<HcsDidMessage> {
+    //     if (decryptionFunction == null) {
+    //         throw "Decryption function is missing or null.";
+    //     }
+    //     return function (encryptedMsg: HcsDidMessage, consensusTimestamp: Timestamp): HcsDidMessage {
+    //         const operation: DidMethodOperation = encryptedMsg.getOperation();
+    //         // Decrypt DID string
+    //         let decryptedDid: string = encryptedMsg.getDid();
+    //         if (decryptedDid != null) {
+    //             const did: string = Hashing.base64.decode(decryptedDid);
+    //             decryptedDid = decryptionFunction(did, consensusTimestamp);
+    //         }
+    //         // Decrypt DID document
+    //         let decryptedDocBase64 = encryptedMsg.getDidDocumentBase64();
+    //         if (decryptedDocBase64 != null) {
+    //             const doc: string = Hashing.base64.decode(decryptedDocBase64);
+    //             decryptedDocBase64 = decryptionFunction(doc, consensusTimestamp);
+    //         }
+    //         return new HcsDidMessage(operation, decryptedDid, decryptedDocBase64);
+    //     };
+    // }
 }
