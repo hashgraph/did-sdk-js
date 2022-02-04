89b0e0f8eccf922d753b33850b97afae3cb7374a
Vijay Shiyani <vshiyani@meeco.me>
2022-02-04 14:55:14 +1100

    added add service method to DID
    Signed-off-by: Derek Munneke (Meeco) <derek.munneke@meeco.me>; Signed-off-by: Derek Munneke <derek.munneke@nextfaze.com>; Signed-off-by: Jan Vereecken <jan.vereecken@meeco.me>; Signed-off-by: Linas Išganaitis <l.isganaitis@gmail.com>; Signed-off-by: Linas Išganaitis <linas.isganaitis@meeco.me>; Signed-off-by: Vijay Shiyani <vshiyani@meeco.me>; Signed-off-by: Zak Barbuto <zbarbuto@nextfaze.com>; Signed-off-by: elena <elena.murova@meeco.me>; Signed-off-by: linasi <l.isganaitis@gmail.com>; Signed-off-by: vijay <vijayshiyani@gmail.com>

--- test/did/hcs-did.js
+++ test/did/hcs-did.js
@@ -199,4 +199,89 @@ describe("HcsDid", function () {
             });
         }).timeout(60000);
     });
+
+    describe("#add-meta-information", async () => {
+        let client;
+
+        before(async () => {
+            const operatorId = AccountId.fromString(OPERATOR_ID);
+            const operatorKey = PrivateKey.fromString(OPERATOR_KEY);
+            client = Client.forTestnet();
+            client.setMirrorNetwork(["hcs." + NETWORK + ".mirrornode.hedera.com:5600"]);
+            client.setOperator(operatorId, operatorKey);
+        });
+
+        it("throws error if privatekey is missing", async () => {
+            const identifier = "did:hedera:testnet:z6MkgUv5CvjRP6AsvEYqSRN7djB6p4zK9bcMQ93g5yK6Td7N_0.0.29613327";
+            const did = new HcsDid({ identifier });
+
+            try {
+                await did.addService({});
+            } catch (err) {
+                assert.instanceOf(err, Error);
+                assert.equal(err.message, "privateKey is missing");
+            }
+        });
+
+        it("throws error if client configuration is missing", async () => {
+            const privateKey = PrivateKey.generate();
+            const did = new HcsDid({ privateKey });
+
+            try {
+                await did.addService({});
+            } catch (err) {
+                assert.instanceOf(err, Error);
+                assert.equal(err.message, "Client configuration is missing");
+            }
+        });
+
+        it("throws error if Service arguments are missing", async () => {
+            const privateKey = PrivateKey.generate();
+            const did = new HcsDid({ privateKey, client });
+
+            try {
+                await did.addService();
+            } catch (err) {
+                assert.instanceOf(err, Error);
+                assert.equal(err.message, "Service args are missing");
+            }
+        });
+
+        it("publish a new Service message", async () => {
+            const privateKey = PrivateKey.fromString(OPERATOR_KEY);
+            const did = new HcsDid({ privateKey, client });
+            await (
+                await did.register()
+            ).addService({
+                id: did.getIdentifier(),
+                type: "LinkedDomains",
+                serviceEndpoint: "https://test.meeco.me/vcs",
+            });
+
+            /**
+             *  wait for 9s so didowner and service event to be propogated to mirror node
+             */
+            await new Promise((resolve) => setTimeout(resolve, 9000));
+
+            console.log(`https://testnet.dragonglass.me/hedera/topics/${did.getTopicId().toString()}`);
+
+            const messages = [];
+
+            new TopicMessageQuery()
+                .setTopicId(did.getTopicId())
+                .setStartTime(new Timestamp(0, 0))
+                .setEndTime(Timestamp.fromDate(new Date()))
+                .subscribe(client, (msg) => {
+                    messages.push(msg);
+                });
+
+            /**
+             * wait for 3s and assume all messages were read
+             */
+            await new Promise((resolve) => setTimeout(resolve, 3000));
+
+            // DIDOwner and Service event
+            assert.equal(messages.length, 2);
+        }).timeout(60000);
+    });
 });
