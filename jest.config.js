51d4f718ff09ce9332bc3995b538a5940be349ce
Vijay Shiyani <vshiyani@meeco.me>
2022-02-07 17:54:15 +1100

    changing it to jest test from mocha
    Signed-off-by: Derek Munneke (Meeco) <derek.munneke@meeco.me>; Signed-off-by: Derek Munneke <derek.munneke@nextfaze.com>; Signed-off-by: Jan Vereecken <jan.vereecken@meeco.me>; Signed-off-by: Linas Išganaitis <l.isganaitis@gmail.com>; Signed-off-by: Linas Išganaitis <linas.isganaitis@meeco.me>; Signed-off-by: Vijay Shiyani <vshiyani@meeco.me>; Signed-off-by: Zak Barbuto <zbarbuto@nextfaze.com>; Signed-off-by: elena <elena.murova@meeco.me>; Signed-off-by: linasi <l.isganaitis@gmail.com>; Signed-off-by: vijay <vijayshiyani@gmail.com>

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnviroment: "node",
+    setupFilesAfterEnv: ["./jest.setup.js"],
+};
--- test/did-document-builder.js
+++
@@ -1,8 +0,0 @@
-const { HcsDid, DidDocumentBuilder, DidDocumentJsonProperties, DidSyntax, Hashing } = require("../dist");
-const { TopicId } = require("@hashgraph/sdk");
-const { expect, assert } = require("chai");
-
-const network = "testnet";
-const DID_TOPIC_ID = TopicId.fromString("0.0.2");
-
-describe("DidDocumentBuilder", function () {});
