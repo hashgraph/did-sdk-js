a1c35f100928eea8b433c7d430b07fd1b52f9017
Vijay Shiyani <vshiyani@meeco.me>
2022-02-07 14:53:21 +1100

    updating, revoking and deleting event message write to hedera

---
+++ demo/5_delete_did_document.js
@@ -0,0 +1,24 @@
+const { PrivateKey, Client } = require("@hashgraph/sdk");
+const { HcsDid } = require("../dist");
+const { OPERATOR_ID, PRIVATE_KEY_STR, TEST_DID_STR } = require("./config");
+
+async function main() {
+    /**
+     * Client setup
+     */
+    const privateKey = PrivateKey.fromString(PRIVATE_KEY_STR);
+    const client = Client.forTestnet();
+    client.setOperator(OPERATOR_ID, privateKey);
+
+    /**
+     * Build DID instance
+     */
+    const did = new HcsDid({ identifier: TEST_DID_STR, privateKey: privateKey, client: client });
+
+    /**
+     * Build create Service message
+     */
+    did.delete();
+}
+
+main();
