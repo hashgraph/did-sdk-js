b35d52635a9c31ba0763dfaac4532a88c18ef843
Linas Išganaitis <l.isganaitis@gmail.com>
2022-02-08 11:43:13 +0100

    Implementation of did delete event
    Signed-off-by: Derek Munneke <derek.munneke@meeco.me>
    
    Signed-off-by: Derek Munneke (Meeco) <derek.munneke@meeco.me>; Signed-off-by: Derek Munneke <derek.munneke@nextfaze.com>; Signed-off-by: Jan Vereecken <jan.vereecken@meeco.me>; Signed-off-by: Linas Išganaitis <l.isganaitis@gmail.com>; Signed-off-by: Linas Išganaitis <linas.isganaitis@meeco.me>; Signed-off-by: Vijay Shiyani <vshiyani@meeco.me>; Signed-off-by: Zak Barbuto <zbarbuto@nextfaze.com>; Signed-off-by: elena <elena.murova@meeco.me>; Signed-off-by: linasi <l.isganaitis@gmail.com>; Signed-off-by: vijay <vijayshiyani@gmail.com>

--- src/identity/hcs/did/event/hcs-did-event-name.ts
+++ src/identity/hcs/did/event/hcs-did-event-name.ts
@@ -3,5 +3,5 @@ export enum HcsDidEventName {
     VERIFICATION_METHOD = "VerificationMethod",
     VERIFICATION_RELATIONSHIP = "VerificationRelationship",
     SERVICE = "Service",
-    DELETE = "Delete",
+    DID = "DID",
 }
