26d9e706cc6dbce6a5f4a63aa1647a022f289564
Vijay Shiyani <vshiyani@meeco.me>
2022-02-10 15:38:01 +1100

    minor fixies and added tests
    Signed-off-by: Derek Munneke (Meeco) <derek.munneke@meeco.me>; Signed-off-by: Derek Munneke <derek.munneke@nextfaze.com>; Signed-off-by: Jan Vereecken <jan.vereecken@meeco.me>; Signed-off-by: Linas Išganaitis <l.isganaitis@gmail.com>; Signed-off-by: Linas Išganaitis <linas.isganaitis@meeco.me>; Signed-off-by: Vijay Shiyani <vshiyani@meeco.me>; Signed-off-by: Zak Barbuto <zbarbuto@nextfaze.com>; Signed-off-by: elena <elena.murova@meeco.me>; Signed-off-by: linasi <l.isganaitis@gmail.com>; Signed-off-by: vijay <vijayshiyani@gmail.com>

--- src/identity/hcs/did/hcs-did-transaction.ts
+++ src/identity/hcs/did/hcs-did-transaction.ts
@@ -1,5 +1,4 @@
 import { TopicId } from "@hashgraph/sdk";
-import { Encrypter } from "../message";
 import { MessageEnvelope } from "../message-envelope";
 import { MessageListener } from "../message-listener";
 import { MessageTransaction } from "../message-transaction";
@@ -17,27 +16,15 @@ export class HcsDidTransaction extends MessageTransaction<HcsDidMessage> {
      * @param topicId The HCS DID topic ID where message will be submitted.
      * @param message The message envelope.
      */
-    constructor(message: MessageEnvelope<HcsDidMessage>, topicId: TopicId);
-    constructor(...args) {
-        if (args[0] instanceof MessageEnvelope && args[1] instanceof TopicId && args.length === 2) {
-            const [message, topicId] = args;
+    constructor(message: MessageEnvelope<HcsDidMessage>, topicId: TopicId) {
+        if (message instanceof MessageEnvelope && topicId instanceof TopicId) {
             super(topicId, message);
         } else {
             throw new Error("Invalid arguments");
         }
     }
 
-    protected buildMessage(): MessageEnvelope<HcsDidMessage> {
-        throw new Error("we no longer submit the whole DID document");
-        // return HcsDidMessage.fromDidDocumentJson(this.didDocument, this.operation);
-    }
-
     protected provideTopicListener(topicIdToListen: TopicId): MessageListener<HcsDidMessage> {
         return new HcsDidTopicListener(topicIdToListen);
-    }
-
-    protected provideMessageEncrypter(encryptionFunction: Encrypter<string>): (input: HcsDidMessage) => HcsDidMessage {
-        throw new Error("we no longer encrypt messages");
-        // return HcsDidMessage.getEncrypter(encryptionFunction);
     }
 }
---
+++ test/unit/hcs-did-transaction.test.ts
@@ -0,0 +1,72 @@
+import { Client, Hbar, PrivateKey, TopicId } from "@hashgraph/sdk";
+import {
+    Hashing,
+    HcsDid,
+    HcsDidMessage,
+    DidMethodOperation,
+    HcsDidCreateDidOwnerEvent,
+    MessageEnvelope,
+    HcsDidTransaction,
+    HcsDidTopicListener,
+} from "../../dist";
+
+describe("HcsDidTransaction", () => {
+    const network = "testnet";
+    const DID_TOPIC_ID1 = TopicId.fromString("0.0.2");
+    const client = Client.forTestnet();
+    const privateKey = PrivateKey.generate();
+    const identifier = `did:hedera:${network}:${Hashing.multibase.encode(
+        privateKey.publicKey.toBytes()
+    )}_${DID_TOPIC_ID1}`;
+    const TRANSACTION_FEE = new Hbar(2);
+
+    describe("execute", () => {
+        const did = new HcsDid({ identifier: identifier, privateKey: privateKey, client: client });
+        const message = new HcsDidMessage(
+            DidMethodOperation.CREATE,
+            "invalid_did###",
+            new HcsDidCreateDidOwnerEvent(
+                did.getIdentifier() + "#did-root-key",
+                did.getIdentifier(),
+                privateKey.publicKey
+            )
+        );
+
+        const envelope = new MessageEnvelope(message);
+        const transaction = new HcsDidTransaction(envelope, DID_TOPIC_ID1);
+
+        it("Validation Errors: signing & Transaction builder function missing", async () => {
+            await expect(transaction.execute(client)).rejects.toThrow(
+                "MessageTransaction execution failed: :\nSigning function is missing.\nTransaction builder is missing."
+            );
+        });
+
+        it("Validation Errors: Transaction builder function missing", async () => {
+            await expect(transaction.signMessage((msg) => privateKey.sign(msg)).execute(client)).rejects.toThrow(
+                "MessageTransaction execution failed: :\nTransaction builder is missing."
+            );
+        });
+
+        //TODO: mock dependencies and test submitTransaction
+
+        // it("", async () => {
+        //     const submitTransaction = await new Promise((resolve, reject) => {
+        //         transaction
+        //             .signMessage((msg) => privateKey.sign(msg))
+        //             .buildAndSignTransaction((tx) => tx.setMaxTransactionFee(HcsDid.TRANSACTION_FEE))
+        //             .onError((err) => {
+        //                 console.error(err);
+        //                 reject(err);
+        //             })
+        //             .onMessageConfirmed((msg) => {
+        //                 console.log("Message Published");
+        //                 console.log(
+        //                     `Explore on DragonGlass: https://testnet.dragonglass.me/hedera/topics/${this.getTopicId()}`
+        //                 );
+        //                 resolve(msg);
+        //             })
+        //             .execute(client);
+        //     });
+        // });
+    });
+});
