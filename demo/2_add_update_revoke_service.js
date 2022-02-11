d01ba37410d7af5a6d1b2eac46b166fe3305af9f
Linas Išganaitis <l.isganaitis@gmail.com>
2022-02-11 15:34:11 +0100

    Move a few demo flow variables to config.js file
    Signed-off-by: Derek Munneke <derek.munneke@meeco.me>
    
    Signed-off-by: Derek Munneke (Meeco) <derek.munneke@meeco.me>; Signed-off-by: Derek Munneke <derek.munneke@nextfaze.com>; Signed-off-by: Jan Vereecken <jan.vereecken@meeco.me>; Signed-off-by: Linas Išganaitis <l.isganaitis@gmail.com>; Signed-off-by: Linas Išganaitis <linas.isganaitis@meeco.me>; Signed-off-by: Vijay Shiyani <vshiyani@meeco.me>; Signed-off-by: Zak Barbuto <zbarbuto@nextfaze.com>; Signed-off-by: elena <elena.murova@meeco.me>; Signed-off-by: linasi <l.isganaitis@gmail.com>; Signed-off-by: vijay <vijayshiyani@gmail.com>

--- demo/2_add_update_revoke_service.js
+++ demo/2_add_update_revoke_service.js
@@ -1,6 +1,6 @@
 const { PrivateKey, Client } = require("@hashgraph/sdk");
 const { HcsDid } = require("../dist");
-const { OPERATOR_ID, OPERATOR_KEY } = require("./config");
+const { OPERATOR_ID, OPERATOR_KEY, DID_IDENTIFIER, DID_PRIVATE_KEY } = require("./config");
 
 async function main() {
     /**
@@ -9,18 +9,12 @@ async function main() {
     const client = Client.forTestnet();
     client.setOperator(OPERATOR_ID, OPERATOR_KEY);
 
-    /**
-     * CHANGE IT. use values from step 1: registered DID console output
-     */
-    const didPrivateKey = PrivateKey.fromString(
-        "302e020100300506032b657004220420a4b76d7089dfd33c83f586990c3a36ae92fb719fdf262e7749d1b0ddd1d055b0"
-    );
-    const existingDIDIdentifier = "did:hedera:testnet:z6MkvD6JAfMyP6pgQoYxfE9rubgwLD9Hmz8rQh1FAxvbW8XB_0.0.29656526";
+    const didPrivateKey = PrivateKey.fromString(DID_PRIVATE_KEY);
 
     /**
      * Build DID instance
      */
-    const registeredDid = new HcsDid({ identifier: existingDIDIdentifier, privateKey: didPrivateKey, client: client });
+    const registeredDid = new HcsDid({ identifier: DID_IDENTIFIER, privateKey: didPrivateKey, client: client });
 
     /**
      * Add Service
